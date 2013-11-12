fe41486531a6cd193e981a95611288d672924501
Gregory Dake <gdake@litle@com>
2013-11-12 13:49:54 -0500

    Version 8.22.1 (Nov 12, 2013)
    Bugfix: Make it simpler to access fields in the accountUpdater response
    without requiring casting and introspection.

--- LitleSdkForNet/LitleSdkForNet/LitleBatchRequest.cs
+++ LitleSdkForNet/LitleSdkForNet/LitleBatchRequest.cs
@@ -817,7 +817,7 @@ namespace Litle.Sdk
             {
                 xmlHeader += "numBalanceInquirys=\"" + numBalanceInquiries + "\"\r\n";
             }
-            xmlHeader += "merchantSdk=\"DotNet;8.22.0\"\r\n";
+            xmlHeader += "merchantSdk=\"DotNet;8.22.1\"\r\n";
 
             xmlHeader += "merchantId=\"" + config["merchantId"] + "\">\r\n";
             return xmlHeader;
