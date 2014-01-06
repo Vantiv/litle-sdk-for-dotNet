b9cb5928006d06bf1bdb6a475ff92612c1e41767
Robert Smieja <rsmieja@litle.com>
2014-01-06 16:31:41 -0500

    Add Program.cs back

---
+++ LitleSdkForNet/LitleSdkForNetTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Litle.Sdk.Test
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
