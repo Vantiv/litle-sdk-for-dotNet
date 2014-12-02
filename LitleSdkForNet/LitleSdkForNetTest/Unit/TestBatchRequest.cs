2d1f6372a75446e70fc554088149e3532bf4ae1c
arch85 <archit.agarwal@gmail.com>
2014-12-02 11:23:36 -0500

    Updating version to 8.27

--- LitleSdkForNet/LitleSdkForNetTest/Unit/TestBatchRequest.cs
+++ LitleSdkForNet/LitleSdkForNetTest/Unit/TestBatchRequest.cs
@@ -55,7 +55,7 @@ namespace Litle.Sdk.Test.Unit
             String actual = batchRequest.generateXmlHeader();
             String expected = @"
 <batchRequest id=""""
-merchantSdk=""DotNet;9.00.0""
+merchantSdk=""DotNet;8.27.0""
 merchantId=""01234"">
 ";
             Assert.AreEqual(expected, actual);
