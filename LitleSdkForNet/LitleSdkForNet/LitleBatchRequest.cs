bb2c8a71da06328f9a1262501eab5357cc7a508b
leonli0326 <leonli0326@gmail.com>
2015-02-23 16:11:54 -0500

    updated version

--- LitleSdkForNet/LitleSdkForNet/LitleBatchRequest.cs
+++ LitleSdkForNet/LitleSdkForNet/LitleBatchRequest.cs
@@ -1231,7 +1231,7 @@ namespace Litle.Sdk
                 xmlHeader += "physicalCheckDebitAmount=\"" + physicalCheckDebitAmount + "\"\r\n";
             }
 
-            xmlHeader += "merchantSdk=\"DotNet;9.3.0\"\r\n";
+            xmlHeader += "merchantSdk=\"DotNet;9.3.1\"\r\n";
 
             xmlHeader += "merchantId=\"" + config["merchantId"] + "\">\r\n";
             return xmlHeader;
--- LitleSdkForNet/LitleSdkForNetTest/Unit/TestBatchRequest.cs
+++ LitleSdkForNet/LitleSdkForNetTest/Unit/TestBatchRequest.cs
@@ -55,7 +55,7 @@ namespace Litle.Sdk.Test.Unit
             String actual = batchRequest.generateXmlHeader();
             String expected = @"
 <batchRequest id=""""
-merchantSdk=""DotNet;9.3.0""
+merchantSdk=""DotNet;9.3.1""
 merchantId=""01234"">
 ";
             Assert.AreEqual(expected, actual);
