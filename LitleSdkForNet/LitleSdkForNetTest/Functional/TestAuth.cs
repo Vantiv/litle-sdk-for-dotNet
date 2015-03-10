38ae57c25c6b192acdb12f7810b57066b9974791
leonli0326 <leonli0326@gmail.com>
2015-03-10 13:58:23 -0400

    fixed bug in test

--- LitleSdkForNet/LitleSdkForNetTest/Functional/TestAuth.cs
+++ LitleSdkForNet/LitleSdkForNetTest/Functional/TestAuth.cs
@@ -15,7 +15,7 @@ namespace Litle.Sdk.Test.Functional
         [TestFixtureSetUp]
         public void SetUpLitle()
         {
-            Dictionary<string, string> config = new Dictionary<string, string>();
+            config = new Dictionary<string, string>();
             config.Add("url", "https://www.testlitle.com/sandbox/communicator/online");
             config.Add("reportGroup", "Default Report Group");
             config.Add("username", "DOTNET");
