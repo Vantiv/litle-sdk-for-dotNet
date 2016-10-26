6601fd29c3daed1d8c941238b3149c6eec40bcef
Brian Arnold <brian.arnold@vantiv.com>
2016-10-26 09:23:30 -0400

    fix negative test for authenticationValue, refactor warnings

--- LitleSdkForNet/LitleSdkForNetTest/Functional/TestSale.cs
+++ LitleSdkForNet/LitleSdkForNetTest/Functional/TestSale.cs
@@ -1,51 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 using NUnit.Framework;
-using Litle.Sdk;
 
 namespace Litle.Sdk.Test.Functional
 {
     [TestFixture]
-    class TestSale
+    internal class TestSale
     {
-        private LitleOnline litle;
+        private LitleOnline _litle;
 
         [TestFixtureSetUp]
-        public void setUp()
+        public void SetUp()
         {
-            Dictionary<string, string> config = new Dictionary<string, string>();
-            config.Add("url", "https://www.testlitle.com/sandbox/communicator/online");
-            
-            config.Add("reportGroup", "Default Report Group");
-            config.Add("username", "DOTNET");
-            config.Add("version", "9.10");
-            config.Add("timeout", "5000");
-            config.Add("merchantId", "101");
-            config.Add("password", "TESTCASE");
-            config.Add("printxml", "true");
-            config.Add("proxyHost", Properties.Settings.Default.proxyHost);
-            config.Add("proxyPort", Properties.Settings.Default.proxyPort);
-            config.Add("logFile", Properties.Settings.Default.logFile);
-            config.Add("neuterAccountNums", "true");
-            litle = new LitleOnline(config);
+            var config = new Dictionary<string, string>
+            {
+                {"url", "https://www.testlitle.com/sandbox/communicator/online"},
+                {"reportGroup", "Default Report Group"},
+                {"username", "DOTNET"},
+                {"version", "9.10"},
+                {"timeout", "5000"},
+                {"merchantId", "101"},
+                {"password", "TESTCASE"},
+                {"printxml", "true"},
+                {"proxyHost", Properties.Settings.Default.proxyHost},
+                {"proxyPort", Properties.Settings.Default.proxyPort},
+                {"logFile", Properties.Settings.Default.logFile},
+                {"neuterAccountNums", "true"}
+            };
+
+            _litle = new LitleOnline(config);
         }
 
         [Test]
         public void SimpleSaleWithCard()
         {
-            sale saleObj = new sale();
-            saleObj.amount = 106;
-            saleObj.litleTxnId = 123456;
-            saleObj.orderId = "12344";
-            saleObj.orderSource = orderSourceType.ecommerce;
-            cardType cardObj = new cardType();
-            cardObj.type = methodOfPaymentTypeEnum.VI;
-            cardObj.number = "4100000000000000";
-            cardObj.expDate = "1210";
-            saleObj.card = cardObj;
-
-            saleResponse responseObj = litle.Sale(saleObj);
+            var saleObj = new sale
+            {
+                amount = 106,
+                litleTxnId = 123456,
+                orderId = "12344",
+                orderSource = orderSourceType.ecommerce,
+                card = new cardType
+                {
+                    type = methodOfPaymentTypeEnum.VI,
+                    number = "4100000000000000",
+                    expDate = "1210"
+                }
+            };
+            
+            var responseObj = _litle.Sale(saleObj);
             StringAssert.AreEqualIgnoringCase("Approved", responseObj.message);
         }
 
@@ -57,17 +59,16 @@ namespace Litle.Sdk.Test.Functional
                 amount = 106,
                 litleTxnId = 123456,
                 orderId = "12344",
-                orderSource = orderSourceType.androidpay
+                orderSource = orderSourceType.androidpay,
+                card = new cardType
+                {
+                    type = methodOfPaymentTypeEnum.VI,
+                    number = "4100000000000000",
+                    expDate = "1210"
+                }
             };
-            var cardObj = new cardType
-            {
-                type = methodOfPaymentTypeEnum.VI,
-                number = "4100000000000000",
-                expDate = "1210"
-            };
-            saleObj.card = cardObj;
-
-            var responseObj = litle.Sale(saleObj);
+           
+            var responseObj = _litle.Sale(saleObj);
             StringAssert.AreEqualIgnoringCase("Approved", responseObj.message);
             Assert.AreEqual("01", responseObj.androidpayResponse.expMonth);
             Assert.AreEqual("2050", responseObj.androidpayResponse.expYear);
@@ -84,17 +85,16 @@ namespace Litle.Sdk.Test.Functional
                 orderSource = orderSourceType.ecommerce,
                 processingType = processingType.accountFunding,
                 originalNetworkTransactionId = "12345678912345",
-                originalTransactionAmount = 1492
+                originalTransactionAmount = 1492,
+                card = new cardType
+                {
+                    type = methodOfPaymentTypeEnum.VI,
+                    number = "4100700000000000",
+                    expDate = "1210"
+                }
             };
-            var cardObj = new cardType
-            {
-                type = methodOfPaymentTypeEnum.VI,
-                number = "4100700000000000",
-                expDate = "1210"
-            };
-            saleObj.card = cardObj;
-
-            var responseObj = litle.Sale(saleObj);
+            
+            var responseObj = _litle.Sale(saleObj);
             StringAssert.AreEqualIgnoringCase("Approved", responseObj.message);
             Assert.AreEqual("123456", responseObj.cardSuffix);
         }
@@ -110,17 +110,16 @@ namespace Litle.Sdk.Test.Functional
                 orderSource = orderSourceType.ecommerce,
                 processingType = processingType.accountFunding,
                 originalNetworkTransactionId = "12345678912345",
-                originalTransactionAmount = 1492
+                originalTransactionAmount = 1492,
+                card = new cardType
+                {
+                    type = methodOfPaymentTypeEnum.VI,
+                    number = "4100700000000000",
+                    expDate = "1210"
+                }
             };
-            var cardObj = new cardType
-            {
-                type = methodOfPaymentTypeEnum.VI,
-                number = "4100700000000000",
-                expDate = "1210"
-            };
-            saleObj.card = cardObj;
-
-            var responseObj = litle.Sale(saleObj);
+            
+            var responseObj = _litle.Sale(saleObj);
             StringAssert.AreEqualIgnoringCase("Approved", responseObj.message);
             Assert.AreEqual("63225578415568556365452427825", responseObj.networkTransactionId);
         }
@@ -136,17 +135,16 @@ namespace Litle.Sdk.Test.Functional
                 orderSource = orderSourceType.ecommerce,
                 processingType = processingType.accountFunding,
                 originalNetworkTransactionId = "12345678912345",
-                originalTransactionAmount = 1492
+                originalTransactionAmount = 1492,
+                card = new cardType
+                {
+                    type = methodOfPaymentTypeEnum.VI,
+                    number = "5400700000000000",
+                    expDate = "1210"
+                }
             };
-            var cardObj = new cardType
-            {
-                type = methodOfPaymentTypeEnum.VI,
-                number = "5400700000000000",
-                expDate = "1210"
-            };
-            saleObj.card = cardObj;
 
-            var responseObj = litle.Sale(saleObj);
+            var responseObj = _litle.Sale(saleObj);
             StringAssert.AreEqualIgnoringCase("Approved", responseObj.message);
             Assert.Null(responseObj.networkTransactionId);
         }
@@ -154,66 +152,79 @@ namespace Litle.Sdk.Test.Functional
         [Test]
         public void SimpleSaleWithMpos()
         {
-            sale saleObj = new sale();
-            saleObj.amount = 106;
-            saleObj.litleTxnId = 123456;
-            saleObj.orderId = "12344";
-            saleObj.orderSource = orderSourceType.ecommerce;
-            mposType mpos = new mposType();
-            mpos.ksn = "77853211300008E00016";
-            mpos.encryptedTrack = "CASE1E185EADD6AFE78C9A214B21313DCD836FDD555FBE3A6C48D141FE80AB9172B963265AFF72111895FE415DEDA162CE8CB7AC4D91EDB611A2AB756AA9CB1A000000000000000000000000000000005A7AAF5E8885A9DB88ECD2430C497003F2646619A2382FFF205767492306AC804E8E64E8EA6981DD";
-            mpos.formatId = "30";
-            mpos.track1Status = 0;
-            mpos.track2Status = 0; ;
-            saleObj.mpos = mpos;
+            var saleObj = new sale
+            {
+                amount = 106,
+                litleTxnId = 123456,
+                orderId = "12344",
+                orderSource = orderSourceType.ecommerce,
+                mpos = new mposType
+                {
+                    ksn = "77853211300008E00016",
+                    encryptedTrack =
+                    "CASE1E185EADD6AFE78C9A214B21313DCD836FDD555FBE3A6C48D141FE80AB9172B963265AFF72111895FE415DEDA162CE8CB7AC4D91EDB611A2AB756AA9CB1A000000000000000000000000000000005A7AAF5E8885A9DB88ECD2430C497003F2646619A2382FFF205767492306AC804E8E64E8EA6981DD",
+                    formatId = "30",
+                    track1Status = 0,
+                    track2Status = 0
+                }
+            };
 
-            saleResponse responseObj = litle.Sale(saleObj);
+            var responseObj = _litle.Sale(saleObj);
             StringAssert.AreEqualIgnoringCase("Approved", responseObj.message);
         }
 
         [Test]
         public void SimpleSaleWithPayPal()
         {
-            sale saleObj = new sale();
-            saleObj.amount = 106;
-            saleObj.litleTxnId = 123456;
-            saleObj.orderId = "12344";
-            saleObj.orderSource = orderSourceType.ecommerce;
-            payPal payPalObj = new payPal();
-            payPalObj.payerId = "1234";
-            payPalObj.token = "1234";
-            payPalObj.transactionId = "123456";
-            saleObj.paypal = payPalObj;
-            saleResponse responseObj = litle.Sale(saleObj);
+            var saleObj = new sale
+            {
+                amount = 106,
+                litleTxnId = 123456,
+                orderId = "12344",
+                orderSource = orderSourceType.ecommerce,
+                paypal = new payPal
+                {
+                    payerId = "1234",
+                    token = "1234",
+                    transactionId = "123456"
+                }
+            };
+            
+            var responseObj = _litle.Sale(saleObj);
             StringAssert.AreEqualIgnoringCase("Approved", responseObj.message);
         }
 
         [Test]
         public void SimpleSaleWithApplepayAndSecondaryAmountAndWallet()
         {
-            sale saleObj = new sale();
-            saleObj.amount = 110;
-            saleObj.secondaryAmount = 50;
-            saleObj.litleTxnId = 123456;
-            saleObj.orderId = "12344";
-            saleObj.orderSource = orderSourceType.ecommerce;
-            applepayType applepay = new applepayType();
-            applepayHeaderType applepayHeaderType = new applepayHeaderType();
-            applepayHeaderType.applicationData = "454657413164";
-            applepayHeaderType.ephemeralPublicKey = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
-            applepayHeaderType.publicKeyHash = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
-            applepayHeaderType.transactionId = "1234";
-            applepay.header = applepayHeaderType;
-            applepay.data = "user";
-            applepay.signature = "sign";
-            applepay.version = "12345";
-            saleObj.applepay = applepay;
-            wallet wallet = new Sdk.wallet();
-            wallet.walletSourceTypeId = "123";
-            wallet.walletSourceType = walletWalletSourceType.MasterPass;
-            saleObj.wallet = wallet;
-
-            saleResponse responseObj = litle.Sale(saleObj);
+            var saleObj = new sale
+            {
+                amount = 110,
+                secondaryAmount = 50,
+                litleTxnId = 123456,
+                orderId = "12344",
+                orderSource = orderSourceType.ecommerce,
+                applepay = new applepayType
+                {
+                    data = "user",
+                    signature = "sign",
+                    version = "12345",
+                    header = new applepayHeaderType
+                    {
+                        applicationData = "454657413164",
+                        ephemeralPublicKey = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+                        publicKeyHash = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+                        transactionId = "1234"
+                    }
+                },
+                wallet = new wallet
+                {
+                    walletSourceTypeId = "123",
+                    walletSourceType = walletWalletSourceType.MasterPass
+                }
+            };
+            
+            var responseObj = _litle.Sale(saleObj);
             Assert.AreEqual("Insufficient Funds", responseObj.message);
             Assert.AreEqual("110", responseObj.applepayResponse.transactionAmount);
         }
@@ -221,23 +232,28 @@ namespace Litle.Sdk.Test.Functional
         [Test]
         public void SimpleSaleWithInvalidFraudCheck()
         {
-            sale saleObj = new sale();
-            saleObj.amount = 106;
-            saleObj.litleTxnId = 123456;
-            saleObj.orderId = "12344";
-            saleObj.orderSource = orderSourceType.ecommerce;
-            cardType cardObj = new cardType();
-            cardObj.type = methodOfPaymentTypeEnum.VI;
-            cardObj.number = "4100000000000000";
-            cardObj.expDate = "1210";
-            saleObj.card = cardObj;
-            fraudCheckType cardholderAuthentication = new fraudCheckType();
-            cardholderAuthentication.authenticationValue = "123456789012345678901234567890123456789012345678901234567890";
-            saleObj.cardholderAuthentication = cardholderAuthentication;
-
+            var saleObj = new sale
+            {
+                amount = 106,
+                litleTxnId = 123456,
+                orderId = "12344",
+                orderSource = orderSourceType.ecommerce,
+                card = new cardType
+                {
+                    type = methodOfPaymentTypeEnum.VI,
+                    number = "4100000000000000",
+                    expDate = "1210"
+                },
+                cardholderAuthentication = new fraudCheckType
+                {
+                    authenticationValue = "1234567890123456789012345678901234567890123456789012345678907897987987897897987897979797897897979797997798789"
+                }
+            };
+            
             try
             {
-                saleResponse responseObj = litle.Sale(saleObj);
+                _litle.Sale(saleObj);
+                Assert.Fail("Exception expected!");
             }
             catch (LitleOnlineException e)
             {
