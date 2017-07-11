47908e103e5a6adf54dcbeb5039b8810633dee78
brianarnold789 <brianarnold789@gmail.com>
2017-07-11 07:18:26 -0400

    update kit script to include dependencies

--- kit.bat
+++ kit.bat
@@ -5,6 +5,12 @@ IF "%1"==[] (
 
 copy LitleSdkForNet\LitleSdkForNet\bin\Release\LitleSdkForNet.dll .\
 copy LitleSdkForNet\LitleSdkForNet\bin\Release\LitleSdkForNet.dll.config .\
-"C:\Program Files\7-Zip\7z.exe" a LitleSdkForNet-%1.zip CHANGELOG LICENSE LitleSdkForNet.dll LitleSdkForNet.dll.config README.md
+copy LitleSdkForNet\LitleSdkForNet\bin\Release\DiffieHellman.dll .\
+copy LitleSdkForNet\LitleSdkForNet\bin\Release\Tamir.SharpSSH.dll .\
+copy LitleSdkForNet\LitleSdkForNet\bin\Release\Org.Mentalis.Security.dll .\
+"C:\Program Files\7-Zip\7z.exe" a LitleSdkForNet-%1.zip CHANGELOG LICENSE LitleSdkForNet.dll LitleSdkForNet.dll.config README.md DiffieHellman.dll Tamir.SharpSSH.dll Org.Mentalis.Security.dll
 del LitleSdkForNet.dll
 del LitleSdkForNet.dll.config
+del DiffieHellman.dll
+del Tamir.SharpSSH.dll
+del Org.Mentalis.Security.dll
