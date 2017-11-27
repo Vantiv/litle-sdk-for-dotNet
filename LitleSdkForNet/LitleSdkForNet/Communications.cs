7c5a661b26ac54bc948e824114d51a3976de3d32
brianarnold789 <brianarnold789@gmail.com>
2017-11-27 15:34:51 -0500

    remove TLSv1.0 from available protocols

--- LitleSdkForNet/LitleSdkForNet/Communications.cs
+++ LitleSdkForNet/LitleSdkForNet/Communications.cs
@@ -89,7 +89,7 @@ namespace Litle.Sdk
             }
             
             var uri = config["url"];
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls; 
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11; 
             var req = (HttpWebRequest)WebRequest.Create(uri);
             
             var neuter = false;
