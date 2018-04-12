d07f2ca9f5b5ef10c6304c4eb841887d99d9d59d
swapnilhande <swapnil.hande@gmail.com>
2018-04-12 17:02:06 -0400

    Remove unused imports

--- LitleSdkForNet/LitleSdkForNet/LitleBatch.cs
+++ LitleSdkForNet/LitleSdkForNet/LitleBatch.cs
@@ -4,9 +4,6 @@ using System.IO;
 using System.Security.Cryptography;
 using System.Xml;
 using System.Xml.Serialization;
-using System.Collections.Generic;
-using System.Runtime.Serialization;
-using System.Xml.Schema;
 
 namespace Litle.Sdk
 {
