af5912223a06242296ee92882485c0cacba5257f
Benji Spetter-Goldstein <benjisg@comcast.net>
2019-08-13 15:37:39 -0400

    fixed tag typo

--- LitleSdkForNet/LitleSdkForNet/Properties/Settings.Designer.cs
+++ LitleSdkForNet/LitleSdkForNet/Properties/Settings.Designer.cs
@@ -61,7 +61,7 @@ namespace Litle.Sdk.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("prelive.litle.com")]
+        [global::System.Configuration.DefaultSettingValueAttribute("payments.vantivprelive.com")]
         public string sftpUrl {
             get {
                 return ((string)(this["sftpUrl"]));
@@ -109,7 +109,7 @@ namespace Litle.Sdk.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("prelive.litle.com")]
+        [global::System.Configuration.DefaultSettingValueAttribute("payments.vantivprelive.com")]
         public string onlineBatchUrl {
             get {
                 return ((string)(this["onlineBatchUrl"]));
@@ -301,8 +301,8 @@ namespace Litle.Sdk.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute(@"")]
-        public string gnuPgDir {
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string GnuPgDir {
             get {
                 return ((string)(this["GnuPgDir"]));
             }
