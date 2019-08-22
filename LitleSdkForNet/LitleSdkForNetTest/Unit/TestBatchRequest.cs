5528f7167d01259e3f4bdd5011fcd3931528c4ee
Vivek Chouhan <vivekchouhan88@gmail.com>
2019-08-22 16:29:39 -0400

    Update TestBatchRequest.cs

--- LitleSdkForNet/LitleSdkForNetTest/Unit/TestBatchRequest.cs
+++ LitleSdkForNet/LitleSdkForNetTest/Unit/TestBatchRequest.cs
@@ -55,7 +55,7 @@ namespace Litle.Sdk.Test.Unit
             var actual = batchRequest.generateXmlHeader();
             var expected = @"
 <batchRequest id=""""
-merchantSdk=""DotNet;11.4.7""
+merchantSdk=""DotNet;11.4.8""
 merchantId=""01234"">
 ";
             
